5a04fd02e074ca5ef1222a7577be7b0393a4a081
Eldad Bercovici <dutzi.b@gmail.com>
2019-09-19 20:55:23 +0300

    Create scroll-conversation-into-view.js

---
+++ scroll-conversation-into-view.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Scroll Conversation Into View
+// @author       You
+// @match        http*://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  let clickCounter = 0;
+  let clickCounterResetTimeout;
+  let isScrolling;
+  let scrollingInterval;
+  let scrollingMessage;
+
+  function handleKeyDown(e) {
+      if (e.key.toLowerCase() === 's') {
+          clearTimeout(clickCounterResetTimeout);
+          clickCounter++;
+          if (clickCounter === 3) {
+              clickCounter = 0;
+              if (isScrolling) {
+                  stopScrolling();
+              } else {
+                  startScrolling();
+              }
+          } else {
+              clickCounterResetTimeout = setTimeout(() => {
+                  clickCounter = 0;
+              }, 500);
+          }
+      }
+  }
+
+  document.addEventListener('keydown', handleKeyDown);
+
+  function addScrollingMessage() {
+    scrollingMessage = document.createElement('div');
+    Object.assign(scrollingMessage.style, {
+      position: 'fixed',
+      top: 0,
+      left:0,
+      right: 0,
+      textAlign: 'center',
+      background: 'red',
+      color: 'white',
+      fontWeight: 'bold',
+      padding: '10px',
+      zIndex: 100000000000
+    });
+    scrollingMessage.innerText = 'Scrolling To Conversation';
+    document.body.appendChild(scrollingMessage)
+  }
+
+  function removeScrollingMessage() {
+    scrollingMessage.parentNode.removeChild(scrollingMessage)
+  }
+
+  function startScrolling() {
+      addScrollingMessage();
+      isScrolling = true;
+      scrollingInterval = setInterval(() => {
+        let conversation;
+        conversation = document.querySelector('[data-conversation-id]') || document.querySelector('[data-spotim-app="conversation"]');
+        if (conversation) {
+          conversation.scrollIntoView();
+          scrollingMessage.innerText = 'Scrolling To Conversation (found!)';
+        } else {
+          scrollingMessage.innerText = 'Scrolling To Conversation (not found)';
+        }
+      }, 100);
+    }
+
+    function stopScrolling() {
+      removeScrollingMessage();
+      clearInterval(scrollingInterval);
+      isScrolling = false;
+  }
+})();
