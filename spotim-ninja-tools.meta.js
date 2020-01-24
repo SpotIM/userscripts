c9479db47df4c17a772d962d8c945ae5e74d6f92
Eldad <eldad@spot.im>
2020-01-24 21:18:58 +0200

    webpack done!

---
+++ spotim-ninja-tools.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        SpotIM Ninja Tools
+// @version     4.0.0
+// @author      dutzi
+// @description A bunch of shortcuts to make our lives easier
+// @match       http*://*/*
+// @namespace   https://spot.im/
+// @noframes    true
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_openInTab
+// @grant       GM_setClipboard
+// @grant       GM_deleteValue
+// @grant       GM_notification
+// @grant       unsafeWindow
+// ==/UserScript==
