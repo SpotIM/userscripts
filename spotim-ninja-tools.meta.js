769ee67b85aaa964d93805af24333d2bf27dbc03
Eldad <eldad@spot.im>
2020-01-26 12:49:16 +0200

    build

--- spotim-ninja-tools.meta.js
+++ spotim-ninja-tools.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        SpotIM Ninja Tools
-// @version     4.0.2
+// @version     4.0.3
 // @author      dutzi
 // @description A bunch of shortcuts to make our lives easier
 // @match       http*://*/*
