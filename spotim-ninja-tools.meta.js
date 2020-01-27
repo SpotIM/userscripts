2b67d973ab626e9f200f3bd43cfbef875c46e4d4
Eldad <eldad@spot.im>
2020-01-27 13:59:56 +0200

    build

--- spotim-ninja-tools.meta.js
+++ spotim-ninja-tools.meta.js
@@ -6,11 +6,14 @@
 // @match       http*://*/*
 // @namespace   https://spot.im/
 // @noframes    true
+// @resource    bigBlueFont https://rawcdn.githack.com/SpotIM/userscripts/53c2ab94cf3523830e17d299ed8abd533822d0c5/ninja-tools/src/assets/BigBlue_Terminal_437TT.TTF
+// @resource    welcomeImage https://github.com/SpotIM/userscripts/raw/master/ninja-tools/welcome-background.png
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_openInTab
 // @grant       GM_setClipboard
 // @grant       GM_deleteValue
 // @grant       GM_notification
+// @grant       GM_getResourceURL
 // @grant       unsafeWindow
 // ==/UserScript==
