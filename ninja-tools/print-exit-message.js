6d8470204d60dc449a80bf89fdd350b6c8b913f7
Eldad <eldad@spot.im>
2020-03-01 15:46:53 +0200

    add npm start messages

---
+++ ninja-tools/print-exit-message.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+console.log(chalk.bold.magenta('（ -.-）ノ-=≡≡卍\n'));
+console.log(
+  chalk.cyan('Now visit ') +
+    chalk.white.underline.bold(
+      'https://github.com/SpotIM/userscripts/raw/master/spotim-ninja-tools.user.js'
+    ) +
+    chalk.cyan(' to re-install the script from Github\n')
+);
---
+++ ninja-tools/print-start-message.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+console.log(chalk.bold.magenta('（ -.-）ノ-=≡≡卍\n'));
+console.log(
+  chalk.cyan('Visit ') +
+    chalk.white.underline.bold(
+      'http://localhost:5732/spotim-ninja-tools.user.js'
+    ) +
+    chalk.cyan(' to install the script\n')
+);
