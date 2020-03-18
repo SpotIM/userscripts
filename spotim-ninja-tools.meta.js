a6cce1f24b64500d9f2f813c600568dfb5157357
Eldad <eldad@spot.im>
2020-03-18 21:33:37 +0200

    release

--- spotim-ninja-tools.meta.js
+++ spotim-ninja-tools.meta.js
@@ -1,13 +1,19 @@
 // ==UserScript==
 // @name        SpotIM Ninja Tools
-// @version     4.1.8
+// @version     4.2.0
 // @author      dutzi
 // @description A bunch of shortcuts to make our lives easier
+// @supportURL  slack://channel?id=CSZGJD6R1&team=T0460KVUF
 // @match       http*://*/*
 // @namespace   https://spot.im/
+// @homepageURL https://github.com/SpotIM/userscripts/tree/master/ninja-tools
 // @noframes    true
 // @resource    bigBlueFont https://rawcdn.githack.com/SpotIM/userscripts/53c2ab94cf3523830e17d299ed8abd533822d0c5/ninja-tools/src/assets/BigBlue_Terminal_437TT.TTF
 // @resource    welcomeImage https://github.com/SpotIM/userscripts/raw/master/ninja-tools/src/assets/welcome-background.png
+// @connect     extract-article-links.dutzi.now.sh
+// @run-at      document-start
+// @iconURL     https://rawcdn.githack.com/SpotIM/userscripts/b327b3d46d1c3bf681a56e05b9b88e6bbcc3153e/ninja-tools/icon64.png
+// @icon64URL   https://rawcdn.githack.com/SpotIM/userscripts/b327b3d46d1c3bf681a56e05b9b88e6bbcc3153e/ninja-tools/icon64.png
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_openInTab
@@ -15,5 +21,8 @@
 // @grant       GM_deleteValue
 // @grant       GM_notification
 // @grant       GM_getResourceURL
+// @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
+// @grant       GM_addValueChangeListener
+// @grant       GM_removeValueChangeListener
 // ==/UserScript==
