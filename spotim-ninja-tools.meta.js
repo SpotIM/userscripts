48e5990834123fdf51347cad79ce697dfac0845c
Eldad <eldad@spot.im>
2020-03-25 21:19:20 +0200

    build

--- spotim-ninja-tools.meta.js
+++ spotim-ninja-tools.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        SpotIM Ninja Tools
-// @version     4.2.3
+// @version     4.2.4
 // @author      dutzi
 // @description A bunch of shortcuts to make our lives easier
 // @supportURL  slack://channel?id=CSZGJD6R1&team=T0460KVUF
@@ -11,6 +11,7 @@
 // @resource    bigBlueFont https://rawcdn.githack.com/SpotIM/userscripts/53c2ab94cf3523830e17d299ed8abd533822d0c5/ninja-tools/src/assets/BigBlue_Terminal_437TT.TTF
 // @resource    welcomeImage https://github.com/SpotIM/userscripts/raw/master/ninja-tools/src/assets/welcome-background.png
 // @connect     extract-article-links.dutzi.now.sh
+// @connect     raw.githubusercontent.com
 // @run-at      document-start
 // @iconURL     https://rawcdn.githack.com/SpotIM/userscripts/b327b3d46d1c3bf681a56e05b9b88e6bbcc3153e/ninja-tools/icon64.png
 // @icon64URL   https://rawcdn.githack.com/SpotIM/userscripts/b327b3d46d1c3bf681a56e05b9b88e6bbcc3153e/ninja-tools/icon64.png
